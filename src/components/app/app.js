47a7364b9d8a59f8923f95a92d34ccca135a99d2
Alex Altn <alex.altn@gmail.com>
2020-03-05 23:01:08 +0200

    структура проекта - каждый компонет в свою папку

---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+
+import './app.css';
+
+const App = () => {
+
+    const todoData = [
+        { label: 'Drink Coffee', important: false, done: true, id: 1 },
+        { label: 'Learn React', important: false, id: 2 },
+        { label: 'Build Awesome React App', important: false, id: 3 },
+        { label: 'Give to the world my Todo App', important: true, id: 4 }
+    ];
+
+    return (
+        <div className="todo-app">            
+            <AppHeader toDo={3} done={1} />
+            <div className="top-panel  d-flex">
+                <SearchPanel /> 
+                <ItemStatusFilter />
+            </div>  
+
+            <TodoList todos={todoData} />
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-import ItemStatusFilter from './components/item-status-filter';
-
-import './index.css';
-
-const App = () => {
-
-    const todoData = [
-        { label: 'Drink Coffee', important: false, done: true, id: 1 },
-        { label: 'Learn React', important: false, id: 2 },
-        { label: 'Build Awesome React App', important: true, id: 3 },
-        { label: 'Give to the world my Todo App', important: false, id: 4 }
-    ];
-
-    return (
-        <div className="todo-app">            
-            <AppHeader toDo={3} done={1} />
-            <div className="top-panel  d-flex">
-                <SearchPanel /> 
-                <ItemStatusFilter />
-            </div>  
-
-            <TodoList todos={todoData} />
-        </div>
-    );
-};
+import App from './components/app';
 
 ReactDOM.render(<App />,
     document.getElementById('root'));

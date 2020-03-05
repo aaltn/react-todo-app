dcfc99bbd5e957d9d7b4bc1c6641a47d6c8dc24b
Alex Altn <alex.altn@gmail.com>
2020-03-05 23:29:35 +0200

    Updates

---
+++ precache-manifest.6d37ec5f941a197be5f2f518c1c2e374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc998e082d06c826b111fe49c2cdd66a",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "5c728d55ec94a4d7f4be",
+    "url": "/app/static/css/main.874b67e8.chunk.css"
+  },
+  {
+    "revision": "10dd6751ed25cb474ea7",
+    "url": "/app/static/js/2.871aca67.chunk.js"
+  },
+  {
+    "revision": "5c728d55ec94a4d7f4be",
+    "url": "/app/static/js/main.1d644874.chunk.js"
+  },
+  {
+    "revision": "2545e4f837a115bcd8be",
+    "url": "/app/static/js/runtime-main.c9d2aecf.js"
+  }
+]);

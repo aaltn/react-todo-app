bcbdc085fdc016d918baf08cf908da1b5c301a0e
Alex Altn <alex.altn@gmail.com>
2020-03-09 21:29:29 +0200

    делает компонент App - компонентом классом, todoData - частью state, создает функцию deleteItem - принимающую id эл.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,29 +7,37 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Learn React', important: false, id: 2 },
-        { label: 'Build Awesome React App', important: false, id: 3 },
-        { label: 'Give to the world my \'Todo\u00A0App\'', important: true, id: 4 }
-    ];
+    state = {
+        todoData: [
+            { label: 'Drink Coffee', important: false, id: 1 },
+            { label: 'Learn React', important: false, id: 2 },
+            { label: 'Build Awesome React App', important: false, id: 3 },
+            { label: 'Give to the world my \'Todo\u00A0App\'', important: true, id: 4 }
+        ]
+    };
 
-    return (
-        <div className="todo-app">            
-            <AppHeader toDo={3} done={1} />
-            <div className="top-panel  d-flex">
-                <SearchPanel /> 
-                <ItemStatusFilter />
-            </div>  
+    deleteItem = (id) => {
+        console.log(id);
+    }
 
-            <TodoList 
-                todos={todoData}
-                onDeleted={ (id) => console.log('del:', id)}
-            />
-        </div>
-    );
+    render() {
+        return (
+            <div className="todo-app">            
+                <AppHeader toDo={3} done={1} />
+                <div className="top-panel  d-flex">
+                    <SearchPanel /> 
+                    <ItemStatusFilter />
+                </div>  
+    
+                <TodoList 
+                    todos={this.state.todoData}
+                    onDeleted={ this.deleteItem }
+                />
+            </div>
+        );
+
+    }
+    
 };
-
-export default App;
